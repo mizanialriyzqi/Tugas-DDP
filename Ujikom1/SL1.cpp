874d38d0c4dfb32f599fceb6627d17147afa2619
Mizani Alriyzqi <mizanialriyzqi@gmail.com>
2025-11-05 10:12:33 +0700

    Mengubah Coding di Dalam File

---
+++ Ujikom1/SL1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Variable Waktu
+    long long detik,detik_awal,sisa_detik,akhir_detik,jam,menit;
+    // Input Untuk Detik Waktu
+    cout << "Masukan Detik Waktu : ";cin>> detik;
+    detik_awal = detik;
+
+    // Konversi Detik Menjadi Waktu Jam + Hari
+    if (detik > 1) {
+        jam = detik / 3600;
+        sisa_detik = detik %= 3600;
+        menit = sisa_detik /60;
+        sisa_detik = sisa_detik %= 60;
+        akhir_detik = sisa_detik;
+
+        // Output Dari Konversi
+        cout << endl;
+        cout << "========================================" << endl;
+        cout << "       Konversi Dari : " << detik_awal << endl; 
+        cout << "========================================" << endl;
+        cout << jam << " JAM, "<<endl;
+        cout << menit << " MENIT, "<< endl;
+        cout << akhir_detik << " DETIK." << endl;
+        cout << "========================================" << endl;
+    } else if(detik <= 1 ) {
+        // Jika Data Tidak Sesuai
+        cout << "Data Input Tidak Valid Brayy!!!!"
+    }
+
+    
+
+
+    return 0;
+}
